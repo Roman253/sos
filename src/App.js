88818c309ed249b45a9e4a318b98fff69c89f39f
Roman253 <romang82@gmail.com>
2022-10-05 13:36:25 +0300

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import emailjs from "emailjs-com";
+import {createRef} from "react";
 
 function App() {
+  let form = createRef()
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+  
+    emailjs.sendForm('service_ubizvfh', 'template_3a4t5mg', form.current, 'user_aPdljovZLcub8EK4W2ZHA')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <form ref={form} onSubmit={sendEmail}>
+      <input type="submit" value="Send" />
+    </form>
+
     </div>
   );
 }
